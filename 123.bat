1bba72af64cab71625324812a4f8fb0c457d822e
yihan1108 <133324620+yihan1108@users.noreply.github.com>
2025-12-27 11:17:35 +0800

    Update 123.bat

--- 123.bat
+++ 123.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
 chcp 65001
 
-SET "DEST_FOLDER=\\192.168.0.17\Public\0Willy\!!!使用手冊%date:~3,4%%date:~8,2%%date:~11,2%%time:~0,2%"
+SET "DEST_FOLDER=C:\Users\user\test%date:~3,4%%date:~8,2%%date:~11,2%%time:~0,2%"
 
-C:\Users\AngelDeng\FastCopy\FastCopy.exe /cmd=diff /auto_close "C:\Users\AngelDeng\OneDrive - 菁華工業股份有限公司\IT" /to="%DEST_FOLDER%"
+C:\Users\AngelDeng\FastCopy\FastCopy.exe /cmd=diff /auto_close "C:\Users\user\databackup" /to="%DEST_FOLDER%"
 CLS
 Exit
+
