5186a001c3765451cd91ae5735e61cdcd2f36157
yihan1108 <133324620+yihan1108@users.noreply.github.com>
2025-12-27 11:32:25 +0800

    Update 123.bat

--- 123.bat
+++ 123.bat
@@ -3,7 +3,8 @@ chcp 65001
 
 SET "DEST_FOLDER=C:\Users\user\test%date:~3,4%%date:~8,2%%date:~11,2%%time:~0,2%"
 
-C:\Users\AngelDeng\FastCopy\FastCopy.exe /cmd=diff /auto_close "C:\Users\user\databackup" /to="%DEST_FOLDER%"
+C:\Users\user\FastCopy\FastCopy.exe /cmd=diff /auto_close "C:\Users\user\databackup" /to="%DEST_FOLDER%"
 CLS
 Exit
+
 
